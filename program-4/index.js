2e99f1d34b343487ca571d6b5db59c1c405b2f76
Sanal Soman <sanal.soman@airindia.com>
2023-09-21 15:06:07 +0530

    fix #4

--- program-4/index.js
+++ program-4/index.js
@@ -4,4 +4,7 @@ function sortAscending(arr) {
     return arr.sort((a, b) => a - b);
 }
 
-console.log(sortAscending(arr)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sortAscending([100,180,260,310,40,535,695])); // outout: [40, 100, 180, 260, 310, 535, 695]
+console.log(sortAscending([1,2,3,4,5,6,7,8,9,10])); // output: [1,2,3,4,5,6,7,8,9,10]
+console.log(sortAscending([10,9,8,7,6,5,4,3,2,1])); // outout: [1,2,3,4,5,6,7,8,9,10]
+
